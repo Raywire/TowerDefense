955ddf78aa1a79009fc6d1a6dff4b4bcc9f42340
Raywire <ryanwire@outlook.com>
2019-09-29 13:29:57 +0300

    Accessor Methods

--- src/Invader.cs
+++ src/Invader.cs
@@ -2,5 +2,16 @@ namespace TreehouseDefense
 {
   class Invader
   {
+    private MapLocation _location;
+
+    public MapLocation GetLocation()
+    {
+      return _location;
+    }
+
+    public void SetLocation(MapLocation value)
+    {
+      _location = value;
+    }
   }
 }
