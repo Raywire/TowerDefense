8c305706365885a39f469ce6e7d8240e9ed6ec0c
Raywire <ryanwire@outlook.com>
2019-09-29 14:28:00 +0300

    Property Practice

--- src/Invader.cs
+++ src/Invader.cs
@@ -8,6 +8,12 @@ namespace TreehouseDefense
 
     public int Health { get; private set; } = 2;
 
+    public bool HasScored { get { return _pathStep >= _path.Length; } }
+
+    public bool IsNeutralized => Health <= 0;
+
+    public bool IsActive => !(IsNeutralized || HasScored);
+
     public Invader(Path path)
     {
       _path = path;
