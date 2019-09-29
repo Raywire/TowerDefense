fe596a27a6a2d921bc9dfa586ec90e03f3b555b6
Raywire <ryanwire@outlook.com>
2019-09-29 10:45:23 +0300

    Encapsulation

--- src/Path.cs
+++ src/Path.cs
@@ -2,5 +2,12 @@ namespace TreehouseDefense
 {
   class Path
   {
+    private readonly MapLocation[] _path;
+    // private MapLocation[] path;
+    public Path(MapLocation[] path)
+    {
+      _path = path;
+      // this.path = path;
+    }
   }
 }
