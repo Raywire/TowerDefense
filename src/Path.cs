c32f476bb4123900d58e4991fd55eca6fef54c36
Raywire <ryanwire@outlook.com>
2019-09-29 11:42:55 +0300

    Index Out of Range

--- src/Path.cs
+++ src/Path.cs
@@ -3,11 +3,21 @@ namespace TreehouseDefense
   class Path
   {
     private readonly MapLocation[] _path;
-    // private MapLocation[] path;
     public Path(MapLocation[] path)
     {
       _path = path;
-      // this.path = path;
+    }
+
+    public GetLocationAt(int pathStep)
+    {
+      if(pathStep < _path.Length)
+      {
+        return _path[pathStep];
+      }
+      else
+      { 
+        return null;
+      }
     }
   }
 }
